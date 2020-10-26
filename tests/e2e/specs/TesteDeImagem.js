69f3f1806551370a82f4ad6f758769d219e416fd
Hudson <hudsonmachado2012@gmail.com>
2020-10-26 18:55:07 -0400

    teste de carregamento de imagem

--- tests/e2e/specs/TesteDeImagem.js
+++ tests/e2e/specs/TesteDeImagem.js
@@ -1,9 +1,16 @@
-cy.request({
-  url: 'https://docs.cypress.io/img/logo.png',
-  encoding: 'base64',
-})
-  .then((response) => {
-    const base64Content = response.body
-    const mime = response.headers['content-type'] // or 'image/png'
-    const imageDataUrl = `data:${mime};base64,${base64Content}`
-  })
+
+describe('Get Image from server', function() {
+  it('successfully loads', function() {
+    cy.request({
+      url: 'http://localhost:8097/perfis/avatar/download?id=1&fileName=capture.jpeg',
+      encoding: 'base64',
+    })
+      .then((response) => {
+        const base64Content = response.body
+        const mime = 'image/png' // or 'image/png'
+        const imageDataUrl = `data:${mime};base64,${base64Content}`
+        debugger
+        cy.writeFile('path/to/save/document.jpg', imageDataUrl, 'base64')
+      })
+  });
+});
